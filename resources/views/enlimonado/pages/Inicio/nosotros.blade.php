61889d75e5cc7724bddad78bb5c07982716b7ced
dsr9 <desarrollador9@tcatik.com>
2025-04-24 15:42:28 +0100

    RS: Pequeño cambio en el inicio

--- resources/views/enlimonado/pages/Inicio/nosotros.blade.php
+++ resources/views/enlimonado/pages/Inicio/nosotros.blade.php
@@ -29,20 +29,6 @@ $valores = [
     'nombre' => 'Maria Lopez',
     'cargo' => 'Directora de Marketing, Empresa Innovación'
   ]
-  [
-    'icono' => '"',
-    'titulo' => 'Nuestra convención pasó de aburrida a icónica en 24h.',
-    'color' => '#B5F613',
-    'usuario' => 'María Lopez',
-    'cargo' => 'Directora de Marketing',
-  ],
-  [
-    'icono' => '"',
-    'titulo' => 'Nunca imaginé que un stand en un centro comercial generara tanto hype.',
-    'color' => '#B5F613',
-    'usuario' => 'Carlos Rodríguez',
-    'cargo' => 'Gerente',
-  ],
 ];
     @endphp
 
@@ -51,16 +37,16 @@ $valores = [
       <div class="p-4 h-100 text-white rounded-3 shadow-sm d-flex flex-column justify-content-between"
       style="background-color: #16121E; border: 1px solid rgba(255, 255, 255, 0.1);">
       <div>
-        <div class="fs-1 mb-3" style="color: {{ $valor['color'] }};">{{ $valor['icono'] }}</div>
-        <h5 class="fw-bold mb-3 text-white">{{ $valor['titulo'] }}</h5>
+        <div class="fs-1 mb-3" style="color: {{ $valor['color'] }};"><i class="ti ti-{{ $valor['icono'] }}"></i></div>
+        <h5 class="fw-bold mb-3 text-white">{{ $valor['texto'] }}</h5>
       </div>
       <div class="d-flex align-items-center">
         <div class="me-3 d-flex justify-content-center align-items-center"
         style="width: 50px; height: 50px; border-radius: 50%; background-color: #8130b1; color: rgb(255, 255, 255); font-weight: bold; font-size: 1.2rem;">
-        {{ substr($valor['usuario'], 0, 1) }}
+        {{ substr($valor['nombre'], 0, 1) }}
         </div>
         <div>
-        <div class="fw-semibold">{{ $valor['usuario'] }}</div>
+        <div class="fw-semibold">{{ $valor['nombre'] }}</div>
         <small class="text-muted">{{ $valor['cargo'] }}</small>
         </div>
       </div>
