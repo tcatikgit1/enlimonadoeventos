03d59988f96e5b9eb7a2dd759601bf9970e4f0f1
dsr9 <desarrollador9@tcatik.com>
2025-05-05 11:13:06 +0100

    RS: Cambios en los h2,h3 de las paginas, filtro de proyectos y metadata en el principalCommonMaster

--- resources/views/web/pages/legals/cookies.blade.php
+++ resources/views/web/pages/legals/cookies.blade.php
@@ -7,7 +7,7 @@
     <!-- Sobre Nosotros Section -->
     <section class="py-7 py-lg-9" style="background-color: #000000;">
       <div class="container">
-        <div class="row justify-content-start" style="margin-top: 9%; margin-bottom: 9%;">
+        <div class="row justify-content-start" style="margin-top: 9%;">
           <div class="col-xl-7 col-lg-8 col-md-10">
             <h1 class="fw-bold display-3 mb-4" style="color: #ffffff;">Política de <span class="fw-bold display-3 mb-4" style="color: #B9FF38;">Cookies</span></h1>
           </div>
@@ -16,7 +16,5 @@
     </section>
 
     @include('web.pages.legals.cookiesconte')
-    <!-- Modal -->
-    @include('_partials._modals.modal-add-role')
 @endsection
 
--- resources/views/web/pages/success/proximoExito.blade.php
+++ resources/views/web/pages/success/proximoExito.blade.php
@@ -5,8 +5,8 @@
 
     <div class="container" style="position: relative; z-index: 2;">
         <div>
-            <h4 class="text-white fs-2 fw-bold pb-2">¿Quieres ser nuestro <span style="color: #B9FF38;">próximo caso de
-                    éxito</span>?</h4>
+            <h2 class="text-white fs-2 fw-bold pb-2">¿Quieres ser nuestro <span style="color: #B9FF38;">próximo caso de
+                    éxito</span>?</h2>
             <p style="color: rgba(255, 255, 255, 0.8); font-size: 21px;">Trabajemos juntos para crear una experiencia que
                 supere todas las expectativas.</p>
             <button class="rounded fw-bold mt-3"
