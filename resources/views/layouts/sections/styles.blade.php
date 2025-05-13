e0b0f4a9f98c560a49e8bfc17b4c2f2b953784ec
dsr9 <desarrollador9@tcatik.com>
2025-05-13 14:48:47 +0100

    RS: añadir las fuentes de publicsans en local

--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -1,15 +1,16 @@
 <!-- BEGIN: Theme CSS-->
 <!-- Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
+{{-- <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"> --}}
 
 @vite([
     'resources/assets/vendor/fonts/tabler-icons.scss',
     'resources/assets/vendor/fonts/fontawesome.scss',
     'resources/assets/vendor/fonts/flag-icons.scss',
     'resources/assets/vendor/libs/node-waves/node-waves.scss',
-    'resources/assets/vendor/scss/pages/ui-carousel.scss'
+    'resources/assets/vendor/scss/pages/ui-carousel.scss',
+    'resources/assets/vendor/fonts/PublicSans.scss'
 
 ])
 <!-- Core CSS -->
--- resources/views/layouts/sections/stylesFront.blade.php
+++ resources/views/layouts/sections/stylesFront.blade.php
@@ -1,11 +1,13 @@
 <!-- BEGIN: Theme CSS-->
 <!-- Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
+{{-- <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet"> --}}
 
 <!-- Icons -->
 @vite(['resources/assets/vendor/fonts/tabler-icons.scss'])
+@vite(['resources/assets/vendor/fonts/PublicSans.scss'])
+
 
 <!-- Core CSS -->
 {{--@vite([--}}
