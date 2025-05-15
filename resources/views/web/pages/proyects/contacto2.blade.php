70044545092e4f27113f2ce48fa63255948a06d3
dsr9 <desarrollador9@tcatik.com>
2025-05-15 09:42:04 +0100

    RS: Cambio de "\" a "/" de una ruta

--- resources/views/web/pages/proyects/contacto2.blade.php
+++ resources/views/web/pages/proyects/contacto2.blade.php
@@ -1,14 +1,12 @@
 @section('page-style')
-    @vite([
-    'resources\scss\styles\components\btnContact.scss',
-    ])
+    @vite(['resources/scss/styles/components/btnContact.scss'])
 @endsection
 
 
 <section class="contact-section py-10" style="background: linear-gradient(to right, #160b24 20%, #000000 80%);">
     <div class="container py-5">
         <div class="text-center mt-5">
-            <h2 class="fw-bold text-white">¿Quieres ser nuestro <span class="lime-text">próximo proyecto</span>?</h2>
+            <h2 class="fw-bold text-white">¿Quieres ser nuestro <span class="lime-text">próximo proyecto<ddd/span>?</h2>
             <p class="text-light fs-5">Cuéntanos qué tienes en mente y hagámoslo realidad juntos.</p>
             <a href="{{ route('contacto') }}" class="btn btn-lime-cta px-4 py-2 rounded" aria-label="Enlace para contactar con Enlimonado">Contáctanos ahora</a>
         </div>
